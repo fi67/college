dbe0bb7213b8ff440c82b9b6ef61b22c9501ed97
fi67 <fi67@users.noreply.github.com>
2019-05-09 01:28:39 +0100

    Update rockPaper.js

--- rockPaper.js
+++ rockPaper.js
@@ -4,24 +4,26 @@ let computerChoice = "";
 let choices = document.getElementsByClassName("choice");
 let cat = document.getElementById("cat");
 let result = "";
+let winner = document.getElementById("winner");
 cat.addEventListener("click", playGame);
 for (let i = 0; i < choices.length; i++) {
-    choices[i].addEventListener("click", playGame);
+    choices[i].addEventListener("click", userPick);
 }
 
-let winner = document.getElementById("winner");
-winner.innerHTML = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
-
-
-
-function playGame(evt) {
+function userPick(evt) {
     userChoice = this.id;
     console.log(userChoice);
+    playGame();
+}
+
+
+function playGame() {
+    
     computerChoice = answers[Math.floor(Math.random() * 3)];
     console.log(computerChoice);
 
 
-    if (userChoice = "cat") {
+    if (userChoice == "cat") {
         result = "meow meow meow MEOW"
     }
 
@@ -29,23 +31,29 @@ function playGame(evt) {
         result = "it's a draw!"
     } else if (userChoice =="rock") {
         if (computerChoice =="scissors") {
-            result ="you win, the computer loses!"
+            result ="you win, as rock blunts scissors!"
         } else {
-            result = "the computer wins, you lose!"
+            result = "the computer wins, as paper wraps rock!"
         }
     } else if (userChoice =="paper") {
         if (computerChoice =="rock") {
-            result = "you win, the computer loses!"
+            result = "you win, as paper wraps rock!!"
         } else {
-            result = "the computer wins, you lose!"
+            result = "the computer wins, as scissors cuts paper!"
         }
     } else if (userChoice =="scissors") {
         if (computerChoice =="paper") {
-            result = "you win, the computer loses!"
+            result = "you win, as scissors cuts paper!!"
         } else {
-            result = "the computer wins, you lose!"
+            result = "the computer wins, as rock blunts scissors!!"
         }
     } 
+  
+displayWinner();
+}
 
+function displayWinner()
+{
+    winner.innerHTML = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
 }
     
