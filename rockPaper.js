4999ee1b59f530b9bb42fe45d8c65db76d071d08
fi67 <fi67@users.noreply.github.com>
2019-05-09 01:00:57 +0100

    Add files via upload

---
+++ rockPaper.js
@@ -0,0 +1,50 @@
+let userChoice;
+let answers = ["rock", "paper", "scissors", "cat"];
+let computerChoice;
+let choices = document.getElementsByClassName("choice");
+let cat= getElementById("cat");
+cat.addEventListener("click", playGame);
+for (let i = 0; i < choices.length; i++) {
+    choices[i].addEventListener("click", playGame);
+}
+
+let winner = document.getElementById("winner");
+winner.innerHTML = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
+
+
+
+function playGame(evt) {
+    userChoice = this.id;
+    console.log(userChoice);
+    computerChoice = answers[Math.floor(Math.random() * 3)];
+    console.log(computerChoice);
+
+
+    if (userChoice = "cat") {
+        result = "meow meow meow MEOW"
+    }
+
+    if (userChoice == computerChoice) {
+        result = "it's a draw!"
+    } else if (userChoice =="rock") {
+        if (computerChoice =="scissors") {
+            result ="you win, the computer loses!"
+        } else {
+            result = "the computer wins, you lose!"
+        }
+    } else if (userChoice =="paper") {
+        if (computerChoice =="rock") {
+            result = "you win, the computer loses!"
+        } else {
+            result = "the computer wins, you lose!"
+        }
+    } else if (userChoice =="scissors") {
+        if (computerChoice =="paper") {
+            result = "you win, the computer loses!"
+        } else {
+            result = "the computer wins, you lose!"
+        }
+    } 
+
+}
+    
