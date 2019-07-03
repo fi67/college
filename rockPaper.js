22b32570e4e93b1209014b334f15dfc9f0e8d9d2
fi67 <fi67@users.noreply.github.com>
2019-07-03 07:47:47 +0100

    Add files via upload

--- rockPaper.js
+++ rockPaper.js
@@ -1,24 +1,15 @@
 //intialise variables
 let userChoice;
-let answers = ["rock", "paper", "scissors", "cat"];
-let computerChoice = "";
-let result = "";
-let choices;
-let cat;
-let catState;
-let winner;
-let myAudio;
 let gameAnswer;
-
 setupInput();
 
 function setupInput() {
-    choices = document.getElementsByClassName("choice");
-    cat = document.getElementById("cat");
-    winner = document.getElementById("winner");
-    myAudio = document.getElementById("myAudio");
-    myAudio.addEventListener("canplaythrough", function(){myAudio.play()});
+    let choices = document.getElementsByClassName("choice");
+    let cat = document.getElementById("cat");
+    let elephant = document.getElementById("elephant");
+    
     cat.addEventListener("click", userPick);
+    elephant.addEventListener("click", userPick);
     for (let i = 0; i < choices.length; i++) {
         choices[i].addEventListener("click", userPick);
     }
@@ -27,7 +18,7 @@ function setupInput() {
 
 function displayOutput()
 {
-
+    let winner = document.getElementById("winner");
     winner.innerHTML = gameAnswer;
 }
 
@@ -38,53 +29,65 @@ function userPick(evt) {
 }
 
 function playGame() {
-    computerChoice = answers[Math.floor(Math.random() * 4)];
+    // web only
+    let myAudio = document.getElementById("myAudio");
+    myAudio.addEventListener("canplaythrough", function(){myAudio.play()});
+    
+    let answers = ["rock", "paper", "scissors", "cat"];
+    let computerChoice = answers[Math.floor(Math.random() * 4)];
     console.log(computerChoice);
-
+    let result="";
+    
     if (userChoice == computerChoice) {
         result = "it's a draw!"
     } else if ((userChoice == "cat") || (computerChoice =="cat")) {
-        catState = Math.floor(Math.random() * 2)
-        console.log(catState)
+        let catState = Math.floor(Math.random() * 2);
+        console.log(catState);
         
         if (userChoice == "cat") {
             if (catState == 0) {
-                result = "The user picked angry cat! The computer wins!"
+                result = "The user picked angry cat! The computer wins!";
                 myAudio.src = "AngryCat.mp3";
             } else {
-                result = "The user picked happy cat! The user wins!"
+                result = "The user picked happy cat! The user wins!";
                 myAudio.src = "Catmeow1.mp3";
             }
         } else {
             if (catState == 0) {
-                result = "The computer picked angry cat! The user wins!"
+                result = "The computer picked angry cat! The user wins!";
                 myAudio.src = "AngryCat.mp3";
             } else {
-                result = "The computer picked happy cat! The computer wins!"
+                result = "The computer picked happy cat! The computer wins!";
                 myAudio.src = "Catmeow1.mp3";
             }
         }
     } else if (userChoice =="rock") {
         if (computerChoice =="scissors") {
-            result ="you win, as rock blunts scissors!"
+            result ="you win, as rock blunts scissors!";
         } else {
-            result = "the computer wins, as paper wraps rock!"
+            result = "the computer wins, as paper wraps rock!";
         }
     } else if (userChoice =="paper") {
         if (computerChoice =="rock") {
-            result = "you win, as paper wraps rock!!"
+            result = "you win, as paper wraps rock!!";
         } else {
-            result = "the computer wins, as scissors cuts paper!"
+            result = "the computer wins, as scissors cuts paper!";
         }
     } else if (userChoice =="scissors") {
         if (computerChoice =="paper") {
-            result = "you win, as scissors cuts paper!!"
+            result = "you win, as scissors cuts paper!!";
         } else {
-            result = "the computer wins, as rock blunts scissors!!"
+            result = "the computer wins, as rock blunts scissors!!";
         }
     } 
 
-    gameAnswer = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
+    if (result === "") {
+        gameAnswer = `User picked ${userChoice}, this is not a valid choice, please try again`;
+        } else {
+        gameAnswer = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
+    }
+       
+    
     
 displayOutput();
 }
