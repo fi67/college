8e441194b388ba200ab723c14ff2c7f0f31a3d3f
fi67 <fi67@users.noreply.github.com>
2019-07-03 10:37:41 +0100

    Update rockPaper.js

--- rockPaper.js
+++ rockPaper.js
@@ -1,92 +1,100 @@
 //intialise variables
+//intialise variables
 let userChoice;
- let gameAnswer;
+let gameAnswer;
 
-setupInput();
+setupWebInput();
 
-function setupInput() {
-    let choices = document.getElementsByClassName("choice");
-    let cat = document.getElementById("cat");
-    let elephant = document.getElementById("elephant");
-    
-    cat.addEventListener("click", userPick);
-    elephant.addEventListener("click", userPick);
-    for (let i = 0; i < choices.length; i++) {
-        choices[i].addEventListener("click", userPick);
-    }
+function setupWebInput() {
+   let choices = document.getElementsByClassName("choice");
+   let cat = document.getElementById("cat");
+   let elephant = document.getElementById("elephant");
+   
+   cat.addEventListener("click", userPick);
+   elephant.addEventListener("click", userPick);
+   for (let i = 0; i < choices.length; i++) {
+       choices[i].addEventListener("click", userPick);
+   }
 
 }
 
 function userPick(evt) {
-    userChoice = this.id;
-    console.log(userChoice);
-    playGame();
+   userChoice = this.id;
+   console.log(userChoice);
+   playGame();
 }
 
 function playGame() {
-    // web only - all things to do with myAudio
-    let myAudio = document.getElementById("myAudio");
-    myAudio.addEventListener("canplaythrough", function(){myAudio.play()});
-    
-    let answers = ["rock", "paper", "scissors", "cat"];
-    let computerChoice = answers[Math.floor(Math.random() * 4)];
-    console.log(computerChoice);
-    let result="";
+   // web only - all things to do with myAudio
+   let myAudio = document.getElementById("myAudio");
+   myAudio.addEventListener("canplaythrough", function(){myAudio.play()});
+   let myCatAudio = "";
+   let answers = ["rock", "paper", "scissors", "cat"];
+   let computerChoice = answers[Math.floor(Math.random() * 4)];
+   console.log(computerChoice);
+   let result="";
+   let catState = 2;
 
-    
-    if (userChoice == computerChoice) {
-        result = "it's a draw!"
-    } else if ((userChoice == "cat") || (computerChoice =="cat")) {
-            
-        let catState = Math.floor(Math.random() * 2);
-        console.log(catState);
-        
-        if (userChoice == "cat") {
-            if (catState == 0) {
-                result = "The user picked angry cat! The computer wins!";
-                myAudio.src = "AngryCat.mp3";
+   
+   if (userChoice === computerChoice) {
+       result = "it's a draw!"
+   } else if ((userChoice === "cat") || (computerChoice =="cat")) {
+           
+       catState = Math.floor(Math.random() * 2);
+       console.log(catState);
+       
+       if (userChoice === "cat") {
+           if (catState === 0) {
+               result = "The user picked angry cat! The computer wins!";
             } else {
-                result = "The user picked happy cat! The user wins!";
-                myAudio.src = "Catmeow1.mp3";
-            }
-        } else {
-            if (catState == 0) {
-                result = "The computer picked angry cat! The user wins!";
-                myAudio.src = "AngryCat.mp3";
+               result = "The user picked happy cat! The user wins!";
+        }
+       } else {
+           if (catState === 0) {
+               result = "The computer picked angry cat! The user wins!";
             } else {
-                result = "The computer picked happy cat! The computer wins!";
-                myAudio.src = "Catmeow1.mp3";
+               result = "The computer picked happy cat! The computer wins!";
             }
-        }
-    } else if (userChoice =="rock") {
-        if (computerChoice =="scissors") {
-            result ="you win, as rock blunts scissors!";
-        } else {
-            result = "the computer wins, as paper wraps rock!";
-        }
-    } else if (userChoice =="paper") {
-        if (computerChoice =="rock") {
-            result = "you win, as paper wraps rock!!";
-        } else {
-            result = "the computer wins, as scissors cuts paper!";
-        }
-    } else if (userChoice =="scissors") {
-        if (computerChoice =="paper") {
-            result = "you win, as scissors cuts paper!!";
-        } else {
-            result = "the computer wins, as rock blunts scissors!!";
-        }
-    } 
+       }
+   } else if (userChoice ==="rock") {
+       if (computerChoice ==="scissors") {
+           result ="you win, as rock blunts scissors!";
+       } else {
+           result = "the computer wins, as paper wraps rock!";
+       }
+   } else if (userChoice ==="paper") {
+       if (computerChoice ==="rock") {
+           result = "you win, as paper wraps rock!!";
+       } else {
+           result = "the computer wins, as scissors cuts paper!";
+       }
+   } else if (userChoice ==="scissors") {
+       if (computerChoice ==="paper") {
+           result = "you win, as scissors cuts paper!!";
+       } else {
+           result = "the computer wins, as rock blunts scissors!!";
+       }
+   } 
 
-    if (result === "") {
-        gameAnswer = `User picked ${userChoice}, this is not a valid choice, please try again`;
-        } else {
-        gameAnswer = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
-    }
-    //web only output to screen
-    let winner = document.getElementById("winner");
-    winner.innerHTML = gameAnswer;
-  }
+   if (result === "") {
+       gameAnswer = `User picked ${userChoice}, this is not a valid choice, please try again`;
+       } else {
+       gameAnswer = `User picked ${userChoice} , computer picked ${computerChoice} - ${result}`;
+   }
+   if ((userChoice === "cat") || (computerChoice =="cat")) {
+     if (catState === 1) {
+        myCatAudio = "Catmeow1.mp3";
+     } else if (catState === 0) {
+        myCatAudio = "AngryCat.mp3";
+     }
+
+   }
+  
+  
+   //web only output to screen
+   let winner = document.getElementById("winner");
+   winner.innerHTML = gameAnswer;
+ }
 
 
     
